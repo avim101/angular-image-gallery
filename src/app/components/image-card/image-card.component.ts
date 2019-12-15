5b9a173257e4746cfb4697dfeb56b759fcd8f13f
Your Nameavim101git config --global user.name Your <avim101@gmail.com>
2019-12-15 07:50:32 +0200

    image card component

---
+++ src/app/components/image-card/image-card.component.ts
@@ -0,0 +1,69 @@
+// ANGULAR
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+// APP
+import { ImageCard } from './image-card.model';
+
+@Component({
+  selector: 'app-image-card',
+  templateUrl: './image-card.component.html',
+  styleUrls: [ './image-card.component.scss' ]
+})
+export class ImageCardComponent implements OnInit {
+  /**
+   * a form to hold component inputs
+   */
+  public titleForm: FormGroup;
+
+  /**
+   * a variable to show / hide popover
+   */
+  public showPopover = false;
+
+  /**
+   * image title
+   */
+  @Input() public image: ImageCard;
+
+  /**
+   * fire event when title changed
+   */
+  @Output() public titleChanged = new EventEmitter<{ image: ImageCard, title: string }>();
+
+  /**
+   * fire event when image is deleted
+   */
+  @Output() public deleted = new EventEmitter<ImageCard>();
+
+  /**
+   * fire event when details icon clicked
+   */
+  @Output() public showMore = new EventEmitter<ImageCard>();
+
+
+  constructor(private formBuilder: FormBuilder) {
+  }
+
+  ngOnInit() {
+    this.titleForm = this.formBuilder.group({
+      title: [ this.image.title, Validators.required ],
+    });
+  }
+
+  public onDeleted(image: ImageCard) {
+    this.deleted.emit(image);
+  }
+
+  public onShowMore(image: ImageCard) {
+    this.showMore.emit(image);
+  }
+
+  submitForm(image: ImageCard) {
+    if (this.titleForm.valid) {
+      this.showPopover = false;
+      // fire title changed with the new value
+      this.titleChanged.emit({ image, title: this.titleForm.controls['title'].value });
+    }
+  }
+}
